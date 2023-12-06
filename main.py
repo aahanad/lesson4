abbdbd9d989cd7972163644f503d2d082f3cb0ba
aahanad <150878761+aahanad@users.noreply.github.com>
2023-12-05 19:07:57 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+disney_characters=["Cinderella","Belle","Ariel","Rapunzel","Mulan","Elsa"]
+print(disney_characters)
+print(disney_characters[4])
+print (len(disney_characters))
+print(disney_characters[0:2])
+print(disney_characters[-3:-1])
+for name in disney_characters:
+    #print(name)
+    if  name == "Rapunzel":
+        print("yes")
